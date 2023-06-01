d3a6d86af189f061a17e852bf2632eadfed36f65
Javascript Enthusiast <abmercy035@gmail.com>
2023-06-01 16:28:46 +0100

    completed the onboarding page

--- src/Layouts/OnboardingLayout.jsx
+++ src/Layouts/OnboardingLayout.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import sliderImgone from "../assets/svg/eventboard.svg"
 import sliderImgtwo from "../assets/svg/boywithcape.svg"
 import sliderImgthree from "../assets/svg/manandwomanstanding.svg"
@@ -7,42 +7,57 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from 'react-responsive-carousel';
 
 export default function OnboardingLayout() {
- const [ carouselTextIndex, setCarouselTextIndex ] = useState(0);
- const carouselText = () => {
-  if (carouselTextIndex <= 0) {
-   console.log('he')
-  }
- }
 
+ const [ count, setCount ] = useState(0);
+
+ useEffect(() => {
+  setInterval(() => {
+   console.log('j')
+  }, 50000)
+ }, [])
 
  class DemoCarousel extends Component {
   render() {
    return (
     <React.Fragment>
-     <Carousel className="onboarding-page-carousel" showThumbs={ false } autoPlay={ true } interval={ 2000 } infiniteLoop={ true } onChange={ () => carouselText() } >
-
-      <div className='carousel-component'>
-       <img src={ sliderImgone } />
-
-      </div>
+     <Carousel className="onboarding-page-carousel" emulateTouch={ false } showArrows={ false } showThumbs={ false } autoPlay={ true } interval={ 3000 } showIndicators={ false } infiniteLoop={ true } stopOnHover={ false }
+     >
       <div className='carousel-component'>
        <img src={ sliderImgone } />
       </div>
       <div className='carousel-component'>
        <img src={ sliderImgtwo } />
       </div>
+      <div className='carousel-component'>
+       <img src={ sliderImgthree } />
+      </div>
      </Carousel>
-     <div className="carousel-text-content">
-      <span className="text-title">Welcome to aKing</span>
-      <p className="text-body">
-       Whats going to happen tomorrow?
-      </p>
-     </div>
+     <Carousel className="carousel-text-content" showArrows={ false } emulateTouch={ false } showThumbs={ false } autoPlay={ true } interval={ 3000 } infiniteLoop={ true } stopOnHover={ false } showStatus={ false } showIndicators={ false }
+     >
+      <div className='carousel-component'>
+       <span className="text-title">Welcome to aKing</span>
+       <p className="text-body">
+        Whats going to happen tomorrow?
+       </p>
+      </div>
+      <div className='carousel-component'>
+       <span className="text-title">Work happens</span>
+       <p className="text-body">
+        Get notified when work happens.
+       </p>
+      </div>
+      <div className='carousel-component'>
+       <span className="text-title">Task and assign </span>
+       <p className="text-body">
+        Task and assign them to colleagues
+       </p>
+      </div>
+     </Carousel>
      <footer>
       <button>Get Started</button>
       <button>Log In</button>
      </footer>
-    </React.Fragment>
+    </React.Fragment >
    );
   }
  };
